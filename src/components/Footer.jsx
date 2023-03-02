0aa9ff747fb704001fe785ebe0cad0ccc572e84b
hardikd-zignuts <hardikd@zignuts.com>
2023-03-02 10:17:20 +0530

    Project Section Hover effect

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,12 @@ import google from "../assets/img/Google.svg";
 const Footer = () => {
   return (
     <>
-      <footer className="footer">
+      <footer id="contact" className="footer">
         <div className="footer-head">
           <div className="footer-head-social">
-            <div className="footer-head-social--title footer-primary-text">Social</div>
+            <div className="footer-head-social--title footer-primary-text">
+              Social
+            </div>
             <div className="footer-head-social--links">
               <img src={facebook} alt="social" />
               <span className="footer-sub-text"> Facebook</span>
@@ -23,20 +25,28 @@ const Footer = () => {
             </div>
           </div>
           <div className="footer-head-explore">
-            <div className="footer-head-explore--title footer-primary-text">Explore</div>
+            <div className="footer-head-explore--title footer-primary-text">
+              Explore
+            </div>
             <div className="footer-sub-text">Services</div>
             <div className="footer-sub-text">Team</div>
             <div className="footer-sub-text">Clients</div>
           </div>
           <div className="footer-head-contact">
-            <div className="footer-head-contact--title footer-primary-text">Contact</div>
+            <div className="footer-head-contact--title footer-primary-text">
+              Contact
+            </div>
             <div className="footer-sub-text">Lorem Ipsum dummy address</div>
             <div className="footer-sub-text">used for display</div>
             <div className="footer-sub-text">1234567890</div>
           </div>
           <div className="footer-head-email">
-            <div className="footer-head-email--title footer-primary-text">Email</div>
-            <div className="footer-sub-text">mendlesoncommunication@email.com</div>
+            <div className="footer-head-email--title footer-primary-text">
+              Email
+            </div>
+            <div className="footer-sub-text">
+              mendlesoncommunication@email.com
+            </div>
           </div>
         </div>
         <div className="footer-bottom">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import BorderBottom from "./BorderBottom";
 import Project1 from "../assets/img/Our Project_ 1 img 1.png";
 import Project2 from "../assets/img/Our Project _ 2 img 1.png";
 import Project3 from "../assets/img/Our Project_ 3 img 1.png";
+import City from "../assets/img/city.jpeg";
 
 const Projects = () => {
   return (
@@ -13,19 +14,31 @@ const Projects = () => {
       <div className="project-gallery">
         <div className="row">
           <div className="col-xl-6 col-lg-12">
-            <div className="mb-3 mb-lg-3 text-xl-end text-lg-center text-md-center">
-              <img className="project-image" src={Project1} alt="project" />
+            <div className="project-body mb-3 mb-lg-3 text-xl-end text-lg-center text-md-center">
+              <img className="project-image" src={City} alt="project" />
+              <div className="project-text">
+                PROJECTS <br />
+                NAME
+              </div>
             </div>
           </div>
           <div className="col-xl-6 col-lg-12">
-            <div className="col-xl-6 col-lg-12">
-              <div className="mb-3 text-lg-center text-md-center">
+            <div className="col-xxl-12 col-xl-12 col-lg-12">
+              <div className="project-body mb-3 text-lg-center text-md-center">
                 <img className="project-image" src={Project2} alt="project" />
+                <div className="project-text">
+                  PROJECTS <br />
+                  NAME
+                </div>
               </div>
             </div>
-            <div className="col-xl-6 col-lg-12">
-              <div className="mt-3 text-lg-center text-md-center">
+            <div className="col-xxl-12 col-xl-12 col-lg-12">
+              <div className="project-body mt-3 text-lg-center text-md-center">
                 <img className="project-image" src={Project3} alt="project" />
+                <div className="project-text">
+                  PROJECTS <br />
+                  NAME
+                </div>
               </div>
             </div>
           </div>
